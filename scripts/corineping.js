39ec5c916b94b2c13e8865ca3e01fab38fba276c
David Porter <dporter@seek.com.au>
2015-09-17 12:54:31 +1000

    added corinebot

---
+++ scripts/corineping.js
@@ -0,0 +1,14 @@
+module.exports = function (robot) {
+
+  robot.hear(/matty/i, function(res) {
+    res.send("is the boss");
+  });
+
+  robot.hear(/ally/i, function(res){
+    res.send("is actually the boss");
+  });
+  robot.hear(/terry/i, function(res){
+    res.send("wishes they were the boss");
+  });
+
+};
